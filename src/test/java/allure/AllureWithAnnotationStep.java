7a2a8ca7aa58b13745a16005f446276dddd014ea
Nadya Frolova <n.frolova.kzn@yandex.ru>
2023-02-19 09:48:52 +0300

    удалены неиспользуемые импорты из AllureWithAnnotationStep

--- src/test/java/allure/AllureWithAnnotationStep.java
+++ src/test/java/allure/AllureWithAnnotationStep.java
@@ -6,9 +6,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class AllureWithAnnotationStep {
     @Test
